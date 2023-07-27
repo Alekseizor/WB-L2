9efac301cc88c639d44aa743b3ca988f9da85863
Alekseizor <LexaCR7@bk.ru>
2023-07-27 15:38:08 +0300

    pattern_05-08

--- pattern/07_strategy.go
+++ pattern/07_strategy.go
@@ -1,1 +1,58 @@
 package pattern
+
+import "log"
+
+type Animal interface {
+	Drink() error
+}
+
+func main7() {
+	cat := NewCat()
+	err := Live("cat", cat)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func Live(name string, animal Animal) error {
+	err := animal.Drink()
+	if err != nil {
+		return err
+	}
+	log.Println(name)
+	return nil
+}
+
+type Cat struct {
+}
+
+func NewCat() *Cat {
+	return &Cat{}
+}
+
+func (c *Cat) Drink() error {
+	return nil
+}
+
+type Dog struct {
+}
+
+func NewDog() *Dog {
+	return &Dog{}
+}
+
+func (c *Dog) Drink() error {
+	return nil
+}
+
+//Применимость паттерна стратегия:
+//1. Когда у вас есть множество похожих классов, отличающихся только некоторым поведением
+//2. Когда вы не хотите обнажать детали реализации алгоритмов для других классов
+//Плюсы паттерна:
+//1. Горячая замена алгоритмов на лету.
+//2. Изолирует код и данные алгоритмов от остальных классов.
+//3. Уход от наследования к делегированию.
+//Минусы паттерна:
+//1. Увеличение сложности кода
+//Реальные примеры использования:
+//1. С моей точки зрения, данный паттерн используется повсеместно в разработке на Go: прокидывание реализации БД для сервиса, различных алгоритмов и так далее.
--- pattern/08_state.go
+++ pattern/08_state.go
@@ -1,1 +1,55 @@
 package pattern
+
+import "fmt"
+
+// Интерфейс состояния
+type State interface {
+	Process() State
+}
+
+// Состояние 1
+type WelcomeState struct{}
+
+func (w *WelcomeState) Process() State {
+	fmt.Println("Hello!")
+	return &OrderCompletionState{}
+}
+
+// Состояние 2
+type OrderCompletionState struct{}
+
+func (o *OrderCompletionState) Process() State {
+	fmt.Println("Thank you for placing an order!")
+	return &WelcomeState{}
+}
+
+// Контекст
+type Client struct {
+	state State
+}
+
+func (c *Client) request() {
+	newState := c.state.Process() // Обработка запроса текущего состояния
+	c.state = newState
+}
+
+func main8() {
+	client := &Client{state: &WelcomeState{}} // Устанавливаем начальное состояние
+
+	// Выполняем несколько запросов для демонстрации изменения состояний
+	client.request()
+	client.request()
+}
+
+//Применимость паттерна стратегия:
+//1. Когда у вас есть объект, поведение которого кардинально меняется в зависимости от внутреннего состояния, причём типов состояний много, и их код часто меняется
+//2. Когда код объекта должен легко расширяться с добавлением новых состояний и/или изменением существующих.
+//Плюсы паттерна:
+//1. Избавляет от множества больших условных операторов машины состояний.
+//2. Концентрирует в одном месте код, связанный с определённым состоянием.
+//Минусы паттерна:
+//1. Может неоправданно усложнить код, если состояний мало и они редко меняются.
+//Реальные примеры использования:
+//1. Чат боты
+//2. Корзина в интернет-магазине
+//3. Различные автоматы по продаже чего-либо,постаматы
