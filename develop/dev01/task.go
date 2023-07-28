769dfb9f6b0842cb52b36fbb87bbb02638cdf3d6
Alekseizor <LexaCR7@bk.ru>
2023-07-28 09:30:45 +0300

    dev_01

--- develop/dev01/task.go
+++ develop/dev01/task.go
@@ -1,1 +1,32 @@
-package dev01
+package main
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/beevik/ntp"
+)
+
+func GetTime(address string) (*time.Time, error) {
+	ntpTime, err := ntp.Time(address)
+	if err != nil {
+		return nil, err
+	}
+	return &ntpTime, nil
+}
+
+func main() {
+	// Получаем точное время с использованием NTP.
+	address := "0.beevik-ntp.pool.ntp.org"
+	ntpTime, err := GetTime(address)
+	if err != nil {
+		log.Println("error when getting time from NTP:", err)
+		os.Exit(1)
+	}
+	// Получаем текущее локальное время.
+	localTime := time.Now()
+	// Выводим текущее время и точное время.
+	log.Println("Текущее время:", localTime.String())
+	log.Println("Точное время: ", ntpTime.String())
+}
--- develop/dev01/task_test.go
+++ develop/dev01/task_test.go
@@ -1,1 +1,35 @@
-package dev01
+package main
+
+import (
+	"testing"
+)
+
+type AddressError struct {
+	address string
+	err     string
+}
+
+func TestGetTimeOK(t *testing.T) {
+	testCases := []string{"0.beevik-ntp.pool.ntp.org"}
+
+	for index, testCase := range testCases {
+		_, err := GetTime(testCase)
+		if err != nil {
+			t.Errorf("[%d] returned error - %v - different from expected - %v", index, err, nil)
+		}
+	}
+}
+func TestGetTimeError(t *testing.T) {
+	testCases := []AddressError{
+		{
+			address: "failAddress",
+			err:     "lookup failAddress on 127.0.0.53:53: server misbehaving",
+		},
+	}
+	for index, testCase := range testCases {
+		_, err := GetTime(testCase.address)
+		if err.Error() != testCase.err {
+			t.Errorf("[%d] returned error - %v - different from expected - %v", index, err, testCase)
+		}
+	}
+}
