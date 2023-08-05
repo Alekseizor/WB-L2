213160d40ee9332e4631ad18ca27f85a111737c7
Alekseizor <LexaCR7@bk.ru>
2023-08-05 16:42:27 +0300

    dev_06-07

--- develop/dev07/task.go
+++ develop/dev07/task.go
@@ -1,1 +1,53 @@
-package dev07
+package main
+
+import (
+	"fmt"
+	"log"
+	"sync"
+	"time"
+)
+
+func composition(channels ...<-chan interface{}) <-chan interface{} {
+	single := make(chan interface{})
+	// Создать горутины для чтения из каждого канала
+	go func() {
+		defer close(single)
+		wg := &sync.WaitGroup{}
+		for _, ch := range channels {
+			wg.Add(1)
+			go func(wg *sync.WaitGroup, c <-chan interface{}) {
+				defer wg.Done()
+				for v := range c {
+					// Передать значение из канала
+					single <- v
+				}
+			}(wg, ch)
+		}
+		wg.Wait()
+	}()
+	return single
+}
+
+func main() {
+	sig := func(after time.Duration) <-chan interface{} {
+		c := make(chan interface{})
+		go func() {
+			defer close(c)
+			time.Sleep(after)
+		}()
+		return c
+	}
+
+	start := time.Now()
+
+	result := composition(
+		sig(3*time.Second),
+		sig(5*time.Second),
+		sig(10*time.Second),
+		sig(12*time.Second),
+	)
+	for res := range result {
+		log.Println(res)
+	}
+	fmt.Printf("Done after %v\n", time.Since(start))
+}
--- develop/dev07/task_test.go
+++ develop/dev07/task_test.go
@@ -1,1 +1,27 @@
-package dev07
+package main
+
+import (
+	"log"
+	"testing"
+	"time"
+)
+
+func TestComposition(t *testing.T) {
+	sig := func(after time.Duration, msg string) <-chan interface{} {
+		c := make(chan interface{})
+		go func() {
+			defer close(c)
+			c <- msg
+			time.Sleep(after)
+		}()
+		return c
+	}
+
+	result := composition(
+		sig(1*time.Second, "hello"),
+		sig(1*time.Second, "wb"),
+	)
+	for res := range result {
+		log.Println(res)
+	}
+}
